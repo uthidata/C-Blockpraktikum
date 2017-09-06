8542757cbe14602f29517d30f6b0a34b030d3a2c
uthidata <ntd.bk.k56@gmail.com>
2017-09-06 02:21:07 +0200

    add car class test

--- Tag2/4.3_car.cpp
+++ Tag2/4.3_car.cpp
@@ -1,17 +1,67 @@
+//g++ -std=c++14 -o 4.3_car.exe 4.3_car.cpp
 #include<iostream>
 
 class Car{
+    
+    double remainingFuel;
+    double drivenLength;
+
 public:
-    Car(double _fuelLevel, double fuelEfficiency);
-    bool refuel (double remainingFuel);
-    double drive (double _distance);
-    double fillLevel (double _fuelLevel);
-    double reach (double _reach);
-    double milage (double _gone);
+    const double capacity;
+    const double fuelEfficiency;
+    Car(double initcap, double initfuelef):capacity(initcap), fuelEfficiency(initfuelef){
+        drivenLength=0;
+        remainingFuel=0;
+    }
+    bool refuel (double tofuel){
+        if(capacity<remainingFuel+tofuel)
+            return 0;
+        else
+            remainingFuel+=tofuel;
+            return 1;
+    }
+    double drive (double distance){
+        if(distance<reach()||abs(distance-reach())<0.00000001){
+            std::cout<<reach()<<" okay "<<distance<<std::endl;
+            drivenLength+=distance;
+            remainingFuel-=distance*fuelEfficiency;
+            return distance;
+        } else if(distance>reach()){
+            std::cout<<reach()<<" no way "<<distance<<std::endl;
+            return reach();
+        }
+    }
+    const double fillLevel (){
+        return remainingFuel;
+    }
+    const double reach (){
+        return remainingFuel/fuelEfficiency;
+    }
+    const double milage (){
+        return drivenLength;
+    }
 private:
-    double fuelEfficiency;
-}
+    
+};
 
 int main(){
-    Car myCar(50,5/100);
+    Car c(50.0,5.0/100.0);
+    std::cout<<c.fillLevel()<<std::endl;
+    std::cout<<c.milage()<<std::endl;
+    c.refuel(20);
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;    
+    c.drive(10);
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.refuel(c.capacity-c.fillLevel());
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.drive(c.reach());
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.drive(1);
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.refuel(20);
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.refuel(c.capacity-c.fillLevel());
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
+    c.refuel(20);
+    std::cout<<c.fillLevel()<<" "<<c.reach()<<" "<<c.milage()<<std::endl;
 }
